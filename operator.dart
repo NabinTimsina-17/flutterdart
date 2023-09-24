96116de2e9848475d29673d8102ccb82fc1e0c45
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-09-24 21:35:47 +0545

    basics of dart

---
+++ operator.dart
@@ -0,0 +1,10 @@
+add(int a,int b){
+  int sum=a+b;
+   return sum;
+}
+void main(){
+  int a=1;
+  int b=2;
+  int c=add(a,b);
+ print("number is:${c}");
+}
---
+++ variables.dart
@@ -0,0 +1,10 @@
+void main(){
+    String name = 'DES VU TECHNOLOGIES';
+    int n = 80;
+    double d = 10.02;
+    bool isGoodCompany=true;
+    print(name);
+    print(n);
+    print(d);
+    print(isGoodCompany);
+}
