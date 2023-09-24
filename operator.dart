68a24e3ef5e96f429a4085801f11643cb8520531
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-09-24 22:33:57 +0545

    operatos in dart

--- operator.dart
+++ operator.dart
@@ -1,10 +1,21 @@
-add(int a,int b){
-  int sum=a+b;
-   return sum;
-}
+// arithmetic operator
+import 'dart:math';
+
 void main(){
-  int a=1;
-  int b=2;
-  int c=add(a,b);
- print("number is:${c}");
+  int a = 10;
+  int b = 11;
+  // int unaryminus = -a;
+  // print(unaryminus);
+  // print(a+b);
+  // print(a-b);
+  // print(a/b);
+  // print(a~/b);
+  // print(a*b);
+// increment and decrement operator
+// print(a--);
+// print(--a);
+// print(a+=b);
+// print(a~/=b);
+// print(a!=b);
+print(a is !String);
 }
